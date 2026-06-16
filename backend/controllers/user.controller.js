46e5738da32ec05622bcb97e5268ed2b846a432a
ARpatel-ARP <rautanky407@gmail.com>
2026-06-16 11:48:09 +0530

    established mongodb

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import {User} from "../models/user.model.js"
+import { json } from "express";
 
 export const register = async (req, res) => {
     try {
@@ -30,6 +31,43 @@ export const register = async (req, res) => {
         })
     } catch (error) {
         console.log(error);
+        return res.status(200).json({
+            success:false,
+            message:"Register failed"
+        })
         
     }
+}
+export const login = async (params) => {
+   try {
+     const {email, password} = req.body;
+     if (!email || !password) {
+         return res.status(400).json({
+             success:false,
+             message:"All fields are required"
+         })
+     }
+     const user = await User.findOne({email})
+     if (!user) {
+        return res.status(400).json({
+            success:false,
+            message:"Incorrect email or password"
+        })
+     }
+     const isPasswordMatch = await bcrypt.compare(password, user.password)
+     if (!isPasswordMatch) {
+        return res.status(400).json({
+            success:false,
+            message:"Incorrect email or password"
+        })
+     }
+     generateToken(res, user, `Welcome Back ${user.name}`)
+   } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+        success:false,
+        message:"Login failed"
+    })
+    
+   }
 }
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,26 @@
-import express from "express";
+import dns from 'node:dns/promises';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from './config/db.js';
+import cookieParser from "cookie-parser"
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors({
+  origin: "http://localhost:5173", // your frontend codespace URL
+  credentials: true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+connectDB()
+app.listen(PORT, () => {
+  console.log(`Server listen at port ${PORT}`);
+})
+  
