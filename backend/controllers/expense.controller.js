72f3265b4b7b51ac5065a5f1fcec35ae576e3b69
ARpatel-ARP <rautanky407@gmail.com>
2026-06-17 15:08:17 +0530

    delete expense (CRUD completed)

--- backend/controllers/expense.controller.js
+++ backend/controllers/expense.controller.js
@@ -1,86 +1,106 @@
-import Expense from "../models/expense.model.js"
+import Expense from "../models/expense.model.js";
 
 export const createExpense = async (req, res) => {
-    try {
-        const {title, amount, category, description, expenseDate} = req.body;
-        if (!title || !amount || !expenseDate) {
-            return res.status(400).json({
-                success:false,
-                message:"Fields are missing"
-            })
-        }
-        const expense = await Expense.create({
-            title,
-            amount,
-            category,
-            description,
-            expenseDate,
-            owner:req.id,
-        })
-        return res.status(200).json({
-            success:true,
-            message:"Expense created successfully"
-        })
-    } catch (error) {
-console.log(error);
-
-        return res.status(400).json({
-            success:false,
-            message:"Failed to create expense"
-        })
-        
+  try {
+    const { title, amount, category, description, expenseDate } = req.body;
+    if (!title || !amount || !expenseDate) {
+      return res.status(400).json({
+        success: false,
+        message: "Fields are missing",
+      });
     }
-}
+    const expense = await Expense.create({
+      title,
+      amount,
+      category,
+      description,
+      expenseDate,
+      owner: req.id,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Expense created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(400).json({
+      success: false,
+      message: "Failed to create expense",
+    });
+  }
+};
 
 export const getExpense = async (req, res) => {
-    try {
-        const userId = req.id;
-        const expenses = await Expense.find({owner:userId})
-        if (!expenses || expenses.length === 0) {
-            return res.status(400).json({
-                success:false,
-                message:'Expense not found'
-            })
-        }
-        return res.status(200).json({
-            success:true,
-            expenses
-        })
-    } catch (error) {
-        return res.status(400).json({
-            success:false,
-            message:"Could not get expenses"
-        })
+  try {
+    const userId = req.id;
+    const expenses = await Expense.find({ owner: userId });
+    if (!expenses || expenses.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Expense not found",
+      });
     }
-}
+    return res.status(200).json({
+      success: true,
+      expenses,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "Could not get expenses",
+    });
+  }
+};
 
 export const updateExpense = async (req, res) => {
+  try {
+    const { expenseId } = req.params;
+    const { title, amount, category, description, expenseDate } = req.body;
+    let updateData = { title, amount, category, description, expenseDate };
+    const expense = await Expense.findByIdAndUpdate(expenseId, updateData, {
+      new: true,
+    });
+    if (!expense) {
+      return res.status(404).json({
+        success: false,
+        message: "Not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Updated successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      success: false,
+      message: "Failed to update",
+    });
+  }
+};
+
+export const deleteExpense = async (req, res) => {
     try {
         const {expenseId} = req.params
-        const {title, amount, category, description, expenseDate} = req.body
-        let updateData = {title, amount, category, description, expenseDate}
-        const expense = await Expense.findByIdAndUpdate(expenseId, updateData, {new:true,})
+        const expense = await Expense.findByIdAndDelete(expenseId)
         if (!expense) {
             return res.status(404).json({
                 success:false,
-                message:"Not found"
+                message:"Expense not found"
             })
         }
-        
         return res.status(200).json({
             success:true,
-            message:"Updated successfully"
+            message:"Deleted successfully"
         })
     } catch (error) {
         console.log(error);
-        return res.status(404).json({
+        return res.status(400).json({
             success:false,
-            message:"Failed to update"
+            message:"Failed to delete"
         })
         
     }
-}
-
-export const deleteExpense = async (req, res) => {
-    
-}
+};
--- backend/routes/expense.routes.js
+++ backend/routes/expense.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createExpense, getExpense, updateExpense } from "../controllers/expense.controller.js"
+import { createExpense, deleteExpense, getExpense, updateExpense } from "../controllers/expense.controller.js"
 import { isAuthenticated } from "../middlewares/isAuthenticated.js"
 
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 router.route('/create').post(isAuthenticated,  createExpense)
 router.route('/').get(isAuthenticated, getExpense)
 router.route('/:expenseId').put(isAuthenticated, updateExpense)
+router.route('/:expenseId').delete(isAuthenticated, deleteExpense)
 
 export default router
