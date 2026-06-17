2665870e4e0b06b1f12fe8369e1daa5c8cb92396
ARpatel-ARP <rautanky407@gmail.com>
2026-06-17 12:09:15 +0530

    expense getter

--- backend/controllers/expense.controller.js
+++ backend/controllers/expense.controller.js
@@ -15,7 +15,7 @@ export const createExpense = async (req, res) => {
             category,
             description,
             expenseDate,
-            userId:req.id,
+            owner:req.id,
         })
         return res.status(200).json({
             success:true,
@@ -30,4 +30,24 @@ export const createExpense = async (req, res) => {
     }
 }
 
-export const 
+export const getExpense = async (req, res) => {
+    try {
+        const userId = req.id;
+        const expenses = await Expense.findOne({owner:userId})
+        if (!expenses) {
+            return res.status(400).json({
+                success:false,
+                message:'Expense not found'
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            expenses
+        })
+    } catch (error) {
+        return res.status(400).json({
+            success:false,
+            message:"Could not get expenses"
+        })
+    }
+}
--- backend/models/expense.model.js
+++ backend/models/expense.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const expenseSchema = new mongoose.Schema({
-    userId:{
+    owner:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
         required:true
